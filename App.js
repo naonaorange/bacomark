51bbb1477113fd6c9fe92d8e17506c32f8a11c15
naonaorange <nao.lunalovegood@gmail.com>
2020-10-09 14:26:10 +0900

    create button function

--- App.js
+++ App.js
@@ -1,21 +1,15 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { Provider } from 'react-redux';
+import { AppScreen } from './screens';
+import { createStore, combineReducers } from 'redux';
+
+const rootReducer = combineReducers({});
+const store = createStore(rootReducer);
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <Provider store={store}>
+      <AppScreen />
+    </Provider>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
---
+++ screens/AppScreen.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import { StyleSheet, Text, SafeAreaView, Button } from 'react-native';
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
+
+const AppScreen = () => {
+  return (
+    <SafeAreaView style={styles.container}>
+      <Button
+        onPress={() => {
+          alert('hello');
+        }}
+        title="hello"
+      />
+    </SafeAreaView>
+  );
+};
+
+export default AppScreen;
